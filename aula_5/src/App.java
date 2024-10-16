53531f84871b30aae3a18528653c9d4c402f3f07
chevitaresebruno <chevitarese.bruno@gmail.com>
2024-10-16 13:58:58 -0300

    Adicionando método de imprimir data com formatação

--- aula_5/src/App.java
+++ aula_5/src/App.java
@@ -7,12 +7,12 @@ import scripts.chore.conta.Corrente;
 import scripts.chore.conta.Poupanca;
 import scripts.chore.usuarios.Gerente;
 import scripts.chore.usuarios.Pessoa;
-import scripts.controllers.conta.ControllerConta;
+import scripts.controllers.adt.ControllerData;
 
 public class App {
     public static void main(String[] args) throws Exception
     {
-        Data dataHoje = new Data(10, 10, 2024);
+        Data dataHoje = new Data(9, 10, 2024);
         Data dataNascimento = new Data(1, 1, 1990);
         Pessoa pessoa1 = new Pessoa("Alice", dataNascimento, 'F', "123.456.789-00");
         Gerente gerente1 = new Gerente("Bob", new Data(5, 5, 1985), 'M', "987.654.321-00", "001", "senha123");
@@ -21,16 +21,7 @@ public class App {
         Poupanca poupanca1 = new Poupanca("P001", pessoa1, dataHoje, gerente1);
 
         // Testando métodos
-        System.out.println("Idade de Alice: " + pessoa1.idade(dataHoje));
-        System.out.println("Idade de Alice: " + pessoa1.idade());
-        System.out.println("Validação de Acesso do Gerente: " + gerente1.validarAcesso("senha123"));
-
-        // Testando rendimentos
-        poupanca1.rendimentos(0.05); // 5% de rendimento
-
-        ControllerConta.sacar(contaCorrente1, contaCorrente1.saldo + 0.01d, "Senha123");
-        contaCorrente1.chequeEspecial(0.05);
-        
-        ControllerConta.extrato(contaCorrente1);
+        ControllerData.imprimir(dataHoje);
+        System.out.print("OI");
     }
 }
--- aula_5/src/scripts/controllers/adt/ControllerData.java
+++ aula_5/src/scripts/controllers/adt/ControllerData.java
@@ -9,14 +9,52 @@ public final class ControllerData
 {
     public static void imprimir(Data data)
     {
-        System.out.println(String.format("%d/%d/%d", data.dia, data.mes, data.ano));
+        imprimir(data, "dd-mm-aaaa");
+    }
+
+    public static void imprimir(Data data, String format)
+    {
+        String[] aux = format.split("-");
+        int length = aux.length - 1;
+
+        switch (length)
+        {
+            case 2 -> { }
+            
+            default -> {
+                System.out.println("ERRO: Atenção, a string de formatação deve estar dividida em três partes com o caractere -");
+                return;            
+            }
+        }
+
+        for(int i = 0; i <= length; i++)
+        {
+            if(aux[i].compareTo("dd") == 0)
+                {
+                    if(data.dia < 10)
+                        { System.out.print("0"); }
+                    System.out.print(String.format("%d/", data.dia));
+                }
+            else if(aux[i].compareTo("mm") == 0)
+                {
+                    if(data.mes < 10)
+                        { System.out.print("0"); }
+                    System.out.print(String.format("%d/", data.mes));
+                }
+            else if(aux[i].compareTo("aaaa") == 0)
+                { System.out.print(String.format("%d/", data.ano)); }
+            else
+                { System.out.println(String.format("\nERRO: string de formatação %s não implementada", aux[i])); return; }
+        }
+
+        System.out.println("\b ");
     }
 
     public static Data today()
     {
         Calendar c = Calendar.getInstance();
 
-        return new Data(c.get(Calendar.DATE), c.get(Calendar.MONTH), c.get(Calendar.YEAR));
+        return new Data(c.get(Calendar.DATE), c.get(Calendar.MONTH)+1, c.get(Calendar.YEAR));
     }
 
     public static Data inputData()
